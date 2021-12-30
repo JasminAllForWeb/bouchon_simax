535c3855e95005e054ae89867b2afa725a0c73f5
Jasmin <contact@visbe.fr>
2021-12-30 17:51:48 +0100

    Remove profiler

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -4,16 +4,15 @@ namespace App\Controller;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use DateTime;
 
 class DeliveryController
 {
     public function indexAction(Request $request/*, LoggerInterface $logger*/): Response
     {
-        //dd($request->query);
-        $number = random_int(0, 100);
+        $date = new DateTime();
+        $dateStr =  $date->format('d/m/y H:i');
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return new Response("Allforweb is OK. $dateStr");
     }
 }
